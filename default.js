fe68429483fdbcd5da9e9f08e0dafa8b664282a0
jesus189970 <68864495+jesus189970@users.noreply.github.com>
2026-03-17 15:21:14 -0400

    Add files via upload

---
+++ default.js
@@ -0,0 +1,28 @@
+// Created on iPad.
+
+
+
+
+// let random = Math.random();
+
+// if(random < 0.5) {
+    // console.log("Your number is less than 0.5")
+    // console.log(random)
+// }
+
+
+// if(random >= 0.5) {
+    // console.log("Your nunmber is greater thn 0.5")
+    // console.log(random)
+// }
+
+// const dayOfWeek = "Friday"
+
+// if(dayOfWeek === "Monday") {
+//     console.log("Ugghh i hatte Mondays")
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("I love Saturdays")
+// } else if (dayOfWeek === "Friday") {
+//     console.log("Fridays are decent")
+// }
+
