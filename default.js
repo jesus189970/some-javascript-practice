42159029f528af59698468aadd7118c7a65950c6
jesus189970 <68864495+jesus189970@users.noreply.github.com>
2026-03-18 14:19:35 -0400

    Add files via upload

--- default.js
+++ default.js
@@ -16,13 +16,31 @@
     // console.log(random)
 // }
 
-// const dayOfWeek = "Friday"
+// const dayOfWeek = prompt('Enter a day.').toLowerCase()
 
-// if(dayOfWeek === "Monday") {
-//     console.log("Ugghh i hatte Mondays")
-// } else if (dayOfWeek === "Saturday") {
+// if(dayOfWeek === "monday") {
+//     console.log("Ugghh i hate Mondays")
+// } else if (dayOfWeek === "saturday") {
 //     console.log("I love Saturdays")
-// } else if (dayOfWeek === "Friday") {
+// } else if (dayOfWeek === "friday") {
 //     console.log("Fridays are decent")
+// } else {
+//     console.log("Meh")
 // }
 
+// 0-5 - free 
+// 5 - 10 - child $10
+// 10 - 65 adult $20
+// 65+ senior $10 
+
+const age = 1;
+
+// if (age < 5) {
+//     console.log('You are a baby, you get in for free')
+// } else if (age < 10) {
+//     console.log('You are a child, you pay $10')
+// } else if (age < 65) {
+//     console.log('You are an adult. You pay $20')
+// } else {
+//     console.log('You are a senior, pay $10')
+// }
